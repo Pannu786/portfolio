88583df79c625b2bb937e299f85b9c9ca8d74677
Pannu786 <thepannu786@gmail.com>
2022-04-18 17:20:03 +0530

    about cpm dne

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,39 @@
 import './about.css';
+import me2 from '../../img/me.png';
+import fcc from '../../img/fcc.png';
 
 const About = () => {
   return (
     <div className='a'>
-      <div className='a-left'></div>
-      <div className='a-right'></div>
+      <div className='a-left'>
+        <div className='a-card bg'></div>
+        <div className='a-card'>
+          <img src={me2} alt='' className='a-img' />
+        </div>
+      </div>
+      <div className='a-right'>
+        <h1 className='a-title'>About Me</h1>
+        <p className='a-sub'>
+          It is a long established fact that a reader will be distracted by the
+          readable content.
+        </p>
+        <p className='a-desc'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+        <div className='a-award'>
+          <img src={fcc} alt='' className='a-award-img' />
+          <div className='a-award-texts'>
+            <h4 className='a-award-title'>FreeCodeCamp</h4>
+            <p className='a-award-desc'>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
+              and fugit.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

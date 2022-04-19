8c8383ba96834d5484cc0d38a983551d09237a6a
Pannu786 <thepannu786@gmail.com>
2022-04-19 21:53:06 +0530

    added data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: 'https://cdn.sanity.io/images/zxpjezk0/production/59a4dcf2821d27273270fe6afa6038264dab5b4e-1892x1055.png',
+    link: 'https://pannu-travelapp.netlify.app/',
+  },
+  {
+    id: 2,
+    img: 'https://cdn.sanity.io/images/zxpjezk0/production/92b0970e96c93b75124ff92bf9298b7668500016-1890x2957.png',
+    link: 'https://realstate-app-two.vercel.app/',
+  },
+  {
+    id: 3,
+    img: 'https://cdn.sanity.io/images/zxpjezk0/production/be62fbd9c014a05ce70cad9ebf5f21bc2014e755-1892x7383.png',
+    link: 'https://pannu-google.netlify.app/news',
+  },
+  {
+    id: 4,
+    img: 'https://cdn.sanity.io/images/zxpjezk0/production/6eb08d3d7e45231c59a890f1b00916375c6c90f7-1880x3058.png',
+    link: 'https://pannu-moviesinfo.netlify.app/',
+  },
+  {
+    id: 5,
+    img: 'https://cdn.sanity.io/images/zxpjezk0/production/cdde21add84cca5993278ba38c710182cc70e75b-1892x955.png',
+    link: 'https://pannu-share-pics-app.netlify.app/create-pin',
+  },
+  {
+    id: 6,
+    img: 'https://cdn.sanity.io/images/zxpjezk0/production/e89672bcf94c7b8104d089f9bbc85f3f31e75b72-1890x3090.png',
+    link: 'http://localhost:3000/',
+  },
+];

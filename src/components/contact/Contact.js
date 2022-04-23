7ec3ab7269ceeb761af891fa981ca8aa9b21c294
Pannu786 <thepannu786@gmail.com>
2022-04-23 22:13:56 +0530

    added style to contact css file

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,18 +6,37 @@ import Address from '../../img/address.png';
 const Contact = () => {
   return (
     <div className='c'>
-      <div className='c-bg'>
-        <div className='c-wrapper'>
-          <div className='c-left'>
-            <h1 className='c-title'>Let's discuss my projects</h1>
-            <div className='c-info'>
-              <div className='c-info-item'>
-                <img src={Phone} alt='' className='c-icon' />
-                +91 9876 543 210
-              </div>
+      <div className='c-bg'></div>
+      <div className='c-wrapper'>
+        <div className='c-left'>
+          <h1 className='c-title'>Let's discuss my projects</h1>
+          <div className='c-info'>
+            <div className='c-info-item'>
+              <img src={Phone} alt='' className='c-icon' />
+              +91 9876 543 210
+            </div>
+            <div className='c-info-item'>
+              <img src={Email} alt='' className='c-icon' />
+              thepannu786@gmail.com
+            </div>
+            <div className='c-info-item'>
+              <img src={Address} alt='' className='c-icon' />
+              Walls st, Wellington, NZ
             </div>
           </div>
-          <div className='c-right'></div>
+        </div>
+        <div className='c-right'>
+          <p className='c-desc'>
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along. me.
+          </p>
+          <form>
+            <input type='text' placeholder='Name' name='user_name' />
+            <input type='text' placeholder='Subjects' name='user_subject' />
+            <input type='text' placeholder='Email' name='user_email' />
+            <textarea rows='5' placeholder='' />
+            <button type='submit'>Submit</button>
+          </form>
         </div>
       </div>
     </div>
